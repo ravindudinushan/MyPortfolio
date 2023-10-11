06ab1809ceb28b1206a6fd738c207bd9edcba231
ravindudinushan <ravindudinushan9966@gmail.com>
2023-10-11 13:10:17 +0530

    POS

--- assignments/JS/Inmemory_POS/controllers/DashboardController.js
+++ assignments/JS/Inmemory_POS/controllers/DashboardController.js
@@ -22,9 +22,6 @@ function setTheLastView() {
         case "ORDERS":
             setView($("#order-page"));
             break;
-        case "ORDER-DETAILS":
-            setView($("#order-detail-page"));
-            break;
         default:
             setView($("#home-page"));
     }
@@ -44,15 +41,6 @@ function saveLastView(clickedID) {
         case "order-page":
             localStorage.setItem("view", "ORDERS");
             break;
-        case "order-detail-page":
-            localStorage.setItem("view", "ORDER-DETAILS");
-            break;
-        case "log-in-page":
-            localStorage.setItem("view", "LOG-IN");
-            break;
-        case "sign-up-page":
-            localStorage.setItem("view", "SIGN-UP");
-            break;
     }
 }
 
@@ -71,7 +59,7 @@ function setView(viewOb) {
 }
 
 function clearAll() {
-    $("#home-page,#customer-page,#item-page,#order-page,#order-detail-page,#log-in-page,#sign-up-page").css('display','none');
+    $("#home-page,#customer-page,#item-page,#order-page").css('display','none');
 }
 
 
@@ -90,150 +78,3 @@ $("#item-nav").click(function () {
 $("#order-nav").click(function () {
     setView($("#order-page"));
 });
-
-$("#order-detail-nav").click(function () {
-    setView($("#order-detail-page"));
-});
-
-$("#log-in-nav").click(function () {
-    setView($("#log-in-page"));
-});
-
-$("#sign-up-nav").click(function () {
-    setView($("#sign-up-page"));
-});
-
-$("#btn-log-in").click(function () {
-    setView($("#home-page"));
-});
-
-$("#btn-sign-up").click(function () {
-    setView($("#log-in-page"));
-});
-
-$("#link-log-in").click(function () {
-    setView($("#log-in-page"));
-});
-
-
-
-// <script>
-//     let imgBody = document.querySelector("body");
-//
-//
-//     let clickHomeNav = document.querySelector("#home-nav");
-//     let clickCustomerNav = document.querySelector("#customer-nav");
-//     let clickItemNav = document.querySelector("#item-nav");
-//     let clickOrderNav = document.querySelector("#order-nav");
-//     let clickOrderDetailNav = document.querySelector("#order-detail-nav");
-//     let clickLogInNav = document.querySelector("#log-in-nav");
-//     let clickSignUpNav = document.querySelector("#sign-up-nav");
-//
-//
-//     let pageHeader = document.querySelector("header");
-//     let pageHome = document.querySelector("#home-page");
-//     let pageCustomer = document.querySelector("#customer-page");
-//     let pageItem = document.querySelector("#item-page");
-//     let pageOrder = document.querySelector("#order-page");
-//     let pageOrderDetail = document.querySelector("#order-detail-page");
-//     let pageLogIn = document.querySelector("#log-in-page");
-//     let pageSignUp = document.querySelector("#sign-up-page");
-//
-//     let btnLogIn = document.querySelector("#btn-log-in");
-//     let btnSignUp = document.querySelector("#btn-sign-up");
-//     let linkLogIn = document.querySelector("#link-log-in");
-//
-//     btnLogIn.addEventListener("click", function (){
-//     pageHeader.style.display='block';
-//     pageHome.style.display='block';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageLogIn.style.display='none';
-// });
-//
-//     btnSignUp.addEventListener("click", function (){
-//     pageLogIn.style.display='block';
-//     pageHeader.style.display='none';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageSignUp.style.display='none';
-// });
-//
-//     linkLogIn.addEventListener("click", function (){
-//     pageLogIn.style.display='block';
-//     pageSignUp.style.display='none';
-// });
-//
-//
-//     clickHomeNav.addEventListener("click", function (){
-//     pageHome.style.display='block';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageLogIn.style.display='none';
-//
-//     clickHomeNav.style.color='black';
-// });
-//
-//     clickCustomerNav.addEventListener("click", function (){
-//     pageCustomer.style.display='block';
-//     pageHome.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageLogIn.style.display='none';
-// });
-//
-//     clickItemNav.addEventListener("click", function (){
-//     pageItem.style.display='block';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageLogIn.style.display='none';
-// });
-//
-//     clickOrderNav.addEventListener("click", function (){
-//     pageOrder.style.display='block';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrderDetail.style.display='none';
-//     pageLogIn.style.display='none';
-// });
-//
-//     clickOrderDetailNav.addEventListener("click", function (){
-//     pageOrderDetail.style.display='block';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageLogIn.style.display='none';
-// });
-//
-//     clickLogInNav.addEventListener("click", function (){
-//     pageLogIn.style.display='block';
-//     pageHeader.style.display='none';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-// });
-//
-//     clickSignUpNav.addEventListener("click", function (){
-//     pageSignUp.style.display='block';
-//     pageHeader.style.display='none';
-//     pageHome.style.display='none';
-//     pageCustomer.style.display='none';
-//     pageItem.style.display='none';
-//     pageOrder.style.display='none';
-//     pageOrderDetail.style.display='none';
-// });
-// </script>
